47bb0360379956a5a43584084372aeb764eb5a83
Frank Arrogo <frankarrogo@github.com>
2022-07-04 17:51:21 +0000

    Task six

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-# 6-print_matrix_integer.py
-
-
 def print_matrix_integer(matrix=[[]]):
-    """Print matrix containing int"""
-    for x in range(len(matrix)):
-        for y in range(len(matrix[x])):
-                print("{:d}".format(matrix[x][y]), end="")
-                if y != (len(matrix[x]) - 1):
-                    print(" ", end="")
-
-        print("")
+    for i in matrix:
+        print(' '.join('{:d}'.format(j)for j in i))
